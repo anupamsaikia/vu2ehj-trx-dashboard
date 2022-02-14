ce2b99185604f96eb527e374f05d2acdebb58c7c
Anupam Saikia <ianupamsaikia@gmail.com>
2022-02-14 07:36:31 +0530

    Make ui for status

--- src/components/Status.js
+++ src/components/Status.js
@@ -7,8 +7,18 @@ export default function Status() {
   const setMessage = useStoreActions((actions) => actions.setMessage);
 
   return (
-    <div>
-      <p class="mt-1 text-sm text-info text-center">
+    <div className="border rounded-lg mt-2">
+      <p className="text-center text-primary font-mono text-sm font-semibold p-1">
+        Device status:{" "}
+      </p>
+      <div className="text-info p-2 text-xs font-mono grid sm:grid-cols-4 xs:grid-cols-1">
+        <p>Mode: CW</p>
+        <p>Tx enabled: false</p>
+        <p>Freq: 7.074 MHz</p>
+        <p>Tx Msg: "CQ VU2EHJ NL66"</p>
+      </div>
+
+      <p class="p-2 text-xs font-mono text-info">
         {message ? `Status: ${message}` : "Status : Clear"}
       </p>
     </div>
